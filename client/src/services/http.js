ea59dac81e0a328bf680808956dfe84711072696
mousamdhakal <mousamdhakal7.md@gmail.com>
2020-11-13 09:28:43 +0545

    Connect frontend to deployed api

--- client/src/services/http.js
+++ client/src/services/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3333/api';
+const BASE_URL = 'http://mern-rest-todo-api.herokuapp.com/api';
 
 export function registerUser(data, callBack) {
   axios
